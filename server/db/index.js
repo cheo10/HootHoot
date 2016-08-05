e4e9681e4349851a3d68be10569ee89323777d0a
Nahee Kwak <naheekw@gmail.com>
2016-08-05 17:44:34 -0400

    Sequelize setup for groups, tables, contacts, channels and test database connection

--- server/db/index.js
+++ server/db/index.js
@@ -12,4 +12,45 @@ var User = db.define('User', {
 
 User.sync();
 
+var Message = db.define('Message', {
+  body: Sequelize.STRING,
+  senderId: Sequelize.STRING,
+  parentMessageId: Sequelize.STRING,
+  originChannelId: Sequelize.STRING,
+});
+
+Message.sync();
+
+var Table = db.define('Table', {
+  name: Sequelize.STRING,
+});
+
+Table.sync();
+
+var GroupRoom = db.define('GroupRoom', {
+  name: Sequelize.STRING,
+  isActive: Sequelize.STRING
+});
+
+GroupRoom.sync();
+
+var Contacts = db.define('Contacts', {
+  name: Sequelize.STRING,
+  isActive: Sequelize.STRING
+});
+
+Contacts.sync();
+
+
+// Declare Relationships between Models
+User.hasMany(Message);
+Message.belongsTo(User);
+
+
+exports.Contacts = Contacts;
+exports.GroupRoom = GroupRoom;
+exports.Table = Table;
+exports.Message = Message;
 exports.User = User;
+
+
