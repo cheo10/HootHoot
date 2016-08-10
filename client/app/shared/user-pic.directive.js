d6a399f733ff3fdfbab6ee862e6250aa390a4981
Forrest Labrum <forrest.labrum@gmail.com>
2016-08-09 18:44:21 -0600

    (refactor) remove some of the auth0 hardcoded values

--- client/app/shared/user-pic.directive.js
+++ client/app/shared/user-pic.directive.js
@@ -1,10 +1,10 @@
 angular.module('userpicdirective', ['theApp']).directive('userpic', function() {
   return {
     restrict: "E",
-    template: '<div class="user-pic"><img src="{{userPicUrl}}" alt="{{senderId}}" class="circle"></div>',
+    template: '<div class="user-pic"><img src="{{userPicUrl}}" alt="{{senderId}}" class="circle userpic"></div>',
 
     controller: function($scope, store){
-      $scope.userPicUrl = store.get('profile').picture;
+      $scope.userPicUrl = localStorage.getItem('picUrl') || 'app/images/person-flat.png';
     }
   };
 });
