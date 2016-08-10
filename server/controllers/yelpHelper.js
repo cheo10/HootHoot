e6f7478c774157f74eec9c56a6da2705d86feb62
Nahee Kwak <naheekw@gmail.com>
2016-08-10 19:55:40 -0400

    Add changes to yelp helpers

--- server/controllers/yelpHelper.js
+++ server/controllers/yelpHelper.js
@@ -1,10 +1,24 @@
 var request = require('request');
 
 exports.searchYelp = function(searchTerm, location) {
- request.get('https://api.yelp.com/v2/search?term=' + searchTerm + '&location=' + location, function(err, res, body){
-    body = JSON.parse(body);
-    return body;
- });
+// Request API access: http://www.yelp.com/developers/getting_started/api_access
+  var Yelp = require('yelp');
+
+  var yelp = new Yelp({
+    consumer_key: 'VahMky6elAwbERS2sapTTQ',
+    consumer_secret: 'iC3WTOgyny37x9qjt7nAfWzWr6U',
+    token: 'kqnSexVLThHXdcgQfocBVNhwxX03IcZJ',
+    token_secret: '0vU63-lLq2tdmL2l9UYFoh_S9Mo',
+  });
+
+  // See http://www.yelp.com/developers/documentation/v2/search_api
+  yelp.search({ term: searchTerm, location: location })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
 };
 
 
