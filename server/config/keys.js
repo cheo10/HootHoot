7642db92e12196f2ded03d91449257ee4fc2af1b
Nahee Kwak <naheekw@gmail.com>
2016-08-16 14:54:20 -0400

    (refactor) remove all private files

--- server/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  oauth_consumer_key : "VahMky6elAwbERS2sapTTQ",
-  oauth_token : "YMQHyPfJxMfGnE1Uu8jb-aeJ1FQM40LM",
-  consumerSecret: "iC3WTOgyny37x9qjt7nAfWzWr6U",
-  tokenSecret: "Uz7tSkgONqIyh7FX3ScwinMk43k"
-}
